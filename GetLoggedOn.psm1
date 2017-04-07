df7787871b44fc2d8a123ccddca32887fc149929
javery45 <javery45@gmail.com>
2017-04-07 13:34:56 -0700

    Uploaded Get-LoggedOn PowerShell module
    This is a PowerShell function that queries a remote system and returns the name of the logged in user. If the returned result is blank, no user is logged in.

---
+++ GetLoggedOn.psm1
@@ -0,0 +1,8 @@
+﻿# Author:  Justin Avery
+# Date:    04/07/2017
+# Description: This is a PowerShell function that queries a remote system and returns the name of the logged in user. If the returned result is blank, no user is logged in.
+
+function Get-LoggedOn ($computerName)
+{
+    Get-WmiObject -Class win32_computersystem -ComputerName $computerName | select username
+}
